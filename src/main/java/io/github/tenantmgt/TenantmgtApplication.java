9d970184a6484fae807c556e9b633c7cabb2dd5d
Md. Mubdiur Rahman <mubdiur@gmail.com>
2022-11-15 08:57:50 +0600

    First commit

---
+++ src/main/java/io/github/tenantmgt/TenantmgtApplication.java
@@ -0,0 +1,13 @@
+package io.github.tenantmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantmgtApplication.class, args);
+	}
+
+}
