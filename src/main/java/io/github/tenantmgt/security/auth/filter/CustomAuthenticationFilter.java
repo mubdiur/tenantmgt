fd53de65f5b7543ecbf0aa60b755ebcb61377f63
Md. Mubdiur Rahman <mubdiur@gmail.com>
2022-11-16 14:35:02 +0600

    added the authorization filter

--- src/main/java/io/github/tenantmgt/security/auth/filter/CustomAuthenticationFilter.java
+++ src/main/java/io/github/tenantmgt/security/auth/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,8 @@ package io.github.tenantmgt.security.auth.filter;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
@@ -19,12 +21,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private static final String APPLICATION_JSON_VALUE = "application/json";
     private final AuthenticationManager authenticationManager;
     private final String jwtSecret;
 
@@ -39,7 +43,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        log.info("Username: {}, Password: {} attempted login", username, password);
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
                 password);
         return authenticationManager.authenticate(authenticationToken);
@@ -73,8 +76,11 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         log.info("Login was successful");
 
-        response.setHeader("access_token", access_token);
-        response.setHeader("refresh_token", refresh_token);
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 
     }
 
--- src/main/java/io/github/tenantmgt/security/config/WebSecurityConfig.java
+++ src/main/java/io/github/tenantmgt/security/config/WebSecurityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import io.github.tenantmgt.security.auth.filter.CustomAuthenticationFilter;
+import io.github.tenantmgt.security.auth.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -43,10 +45,22 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // http.authorizeRequests().anyRequest().permitAll();
+        CustomAuthenticationFilter customAuthenticationFilter = 
+                new CustomAuthenticationFilter(authManagerBuilder.getOrBuild(), jwtSecret);
+        
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login/**");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authManagerBuilder.getOrBuild(), jwtSecret));
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/register/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/tenant/**").hasAnyAuthority("ROLE_TENANT");
+        http.authorizeRequests().antMatchers("/api/admin/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/api/owner/**").hasAnyAuthority("ROLE_OWNER");
+        http.authorizeRequests().antMatchers("/api/manager/**").hasAnyAuthority("ROLE_MANAGER");
+        http.authorizeRequests().anyRequest().authenticated();
+
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(jwtSecret), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
