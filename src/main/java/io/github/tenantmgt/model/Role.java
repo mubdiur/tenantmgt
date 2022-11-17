5822da905cc444b02973467f4ad1a0e4d6f53fd8
Md. Mubdiur Rahman <mubdiur@gmail.com>
2022-11-18 03:17:19 +0600

    import cleanup

--- src/main/java/io/github/tenantmgt/model/Role.java
+++ src/main/java/io/github/tenantmgt/model/Role.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import static javax.persistence.GenerationType.*;
 
-import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
--- src/main/java/io/github/tenantmgt/model/User.java
+++ src/main/java/io/github/tenantmgt/model/User.java
@@ -3,7 +3,6 @@ package io.github.tenantmgt.model;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
